c04252aa8a6e01c933bec8553207027be65f5b32
orzarhi <or.zarhi11@gmail.com>
2024-07-31 01:32:31 +0300

    chore: Add utility function for merging class names

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import { Navbar } from "@/components/Navbar";
+import { Providers } from "@/components/Providers";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <Providers>{children}</Providers>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,6 @@
-import Image from "next/image";
-import {
-  RegisterLink,
-  LoginLink,
-  LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LoginLink>Sign in</LoginLink>
-
-      <RegisterLink>Sign up</RegisterLink>
-
-      <LogoutLink>Sign out</LogoutLink>
-    </main>
+<main></main>
   );
 }

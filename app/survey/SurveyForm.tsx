af1f00b09e3a6f3d7d7fdfd5a46d72f9df281a96
orzarhi <or.zarhi11@gmail.com>
2024-08-05 22:29:05 +0300

    feat: Add SATISFACTION constant to config/index.ts
    - Add SATISFACTION constant to config/index.ts file to define the different levels of satisfaction.
    - This change provides a standardized set of values for satisfaction levels in the application.

--- app/survey/SurveyForm.tsx
+++ app/survey/SurveyForm.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -10,14 +13,58 @@ import {
   SelectValue,
 } from '@/components/ui/select';
 import { Textarea } from '@/components/ui/textarea';
-import { Survey } from '@prisma/client';
+import { QuestionType, Survey } from '@prisma/client';
+import { useCallback } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { SATISFACTION } from '@/config';
+
+type satisfaction = keyof typeof SATISFACTION;
+
+type FormData = {
+  feedback: string;
+  satisfaction: satisfaction;
+  [key: `question-${number}`]: string;
+  [key: `question-${number}-answer-${number}`]: boolean;
+};
+
+const formatSubmissionData = (data: FormData, survey: SurveyFormProps['survey']) => {
+  return {
+    feedback: data.feedback,
+    satisfaction: data.satisfaction,
+    questions: survey.questions.map((question) => {
+      const questionId = question.id;
+      if (question.questionType === 'SINGLE_CHOICE') {
+        const selectedAnswer = question.answers.find(
+          (answer) => answer.text === data[`question-${questionId}`]
+        );
+        return {
+          questionId,
+          answer: selectedAnswer
+            ? [{ answerId: selectedAnswer.id, text: selectedAnswer.text }]
+            : [],
+        };
+      } else if (question.questionType === 'MULTIPLE_CHOICE') {
+        const selectedAnswers = question.answers
+          .filter((answer) => data[`question-${questionId}-answer-${answer.id}`])
+          .map((answer) => ({ answerId: answer.id, text: answer.text }));
+        return { questionId, answer: selectedAnswers };
+      } else if (question.questionType === 'SHORT_ANSWER') {
+        return {
+          questionId,
+          answer: [{ answerId: null, text: data[`question-${questionId}`] }],
+        };
+      }
+      return { questionId, answer: [] };
+    }),
+  };
+};
 
 interface SurveyFormProps {
   survey: Survey & {
     questions: {
       id: number;
       text: string;
-      questionType: 'SINGLE_CHOICE' | 'MULTIPLE_CHOICE' | 'SHORT_ANSWER';
+      questionType: QuestionType;
       answers: {
         id: number;
         text: string;
@@ -26,53 +73,107 @@ interface SurveyFormProps {
   };
 }
 
+const LABEL_MAP: Record<satisfaction, string> = {
+  VERY_SATISFIED: 'Very Satisfied',
+  SATISFIED: 'Satisfied',
+  NEUTRAL: 'Neutral',
+  DISSATISFIED: 'Dissatisfied',
+  VERY_DISSATISFIED: 'Very Dissatisfied',
+};
+
+
 export const SurveyForm = ({ survey }: SurveyFormProps) => {
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = useCallback(
+    (data: any) => {
+      const formattedData = formatSubmissionData(data, survey);
+      console.log(formattedData);
+    },
+    [survey]
+  );
+
   return (
-    <form className="mt-8 space-y-8">
+    <form className="mt-8 space-y-8" onSubmit={handleSubmit(onSubmit)}>
       <div className="space-y-4">
         {survey.questions.map((question, index) => (
           <div key={question.id} className="space-y-2">
-            {index + 1}. <Label htmlFor={`question-${index}`}>{question.text}</Label>
-            {question.questionType === 'SINGLE_CHOICE' ? (
-              <RadioGroup key={index} name={`question-${index}`}>
-                {question.answers.map((answer, answerIndex) => (
-                  <div key={answer.id} className="flex items-center space-x-2">
-                    <RadioGroupItem
-                      value={answer.text}
-                      id={`question-${index}-answer-${answerIndex}`}
-                    />
-                    <Label htmlFor={`question-${index}-answer-${answerIndex}`}>
-                      {answer.text}
-                    </Label>
-                  </div>
-                ))}
-              </RadioGroup>
-            ) : (
-              question.answers.map((answer, answerIndex) => {
-                if (question.questionType === 'SHORT_ANSWER') {
-                  return (
-                    <Input
-                      key={answerIndex}
-                      id={`question-${index}-answer-${answerIndex}`}
-                      name={`question-${index}`}
-                      placeholder={answer.text}
-                    />
-                  );
-                } else if (question.questionType === 'MULTIPLE_CHOICE') {
-                  return (
-                    <div key={answerIndex} className="flex items-center space-x-2">
+            {index + 1}.{' '}
+            <Label htmlFor={`question-${question.id}`}>{question.text}</Label>
+            {question.questionType === 'SINGLE_CHOICE' && (
+              <>
+                <Controller
+                  name={`question-${question.id}`}
+                  control={control}
+                  rules={{ required: 'Answer is required' }}
+                  render={({ field }) => (
+                    <RadioGroup onValueChange={field.onChange} value={field.value}>
+                      {question.answers.map((answer) => (
+                        <div key={answer.id} className="flex items-center space-x-2">
+                          <RadioGroupItem
+                            value={answer.text}
+                            id={`question-${question.id}-answer-${answer.id}`}
+                          />
+                          <Label htmlFor={`question-${question.id}-answer-${answer.id}`}>
+                            {answer.text}
+                          </Label>
+                        </div>
+                      ))}
+                    </RadioGroup>
+                  )}
+                />
+                {errors[`question-${question.id}`] && (
+                  <p className="error_message">
+                    {errors[`question-${question.id}`]?.message?.toString()}
+                  </p>
+                )}
+              </>
+            )}
+            {question.questionType === 'MULTIPLE_CHOICE' &&
+              question.answers.map((answer) => (
+                <div key={answer.id} className="flex items-center space-x-2">
+                  <Controller
+                    name={`question-${question.id}-answer-${answer.id}`}
+                    control={control}
+                    rules={{ required: 'Answer is required' }}
+                    render={({ field }) => (
                       <Checkbox
-                        id={`question-${index}-answer-${answerIndex}`}
-                        name={`question-${index}`}
-                        value={answer.text}
+                        id={`question-${question.id}-answer-${answer.id}`}
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
                       />
-                      <Label htmlFor={`question-${index}-answer-${answerIndex}`}>
-                        {answer.text}
-                      </Label>
-                    </div>
-                  );
-                }
-              })
+                    )}
+                  />
+                  <Label htmlFor={`question-${question.id}-answer-${answer.id}`}>
+                    {answer.text}
+                  </Label>
+                </div>
+              ))}
+            {errors[`question-${question.id}`] && (
+              <p className="error_message">
+                {errors[`question-${question.id}`]?.message?.toString()}
+              </p>
+            )}
+            {question.questionType === 'SHORT_ANSWER' && (
+              <>
+                <Input
+                  id={`question-${question.id}`}
+                  {...register(`question-${question.id}`, {
+                    required: 'Answer is required',
+                  })}
+                  placeholder="Enter your answer"
+                />
+                {errors[`question-${question.id}`] && (
+                  <p className="error_message">
+                    {errors[`question-${question.id}`]?.message?.toString()}
+                  </p>
+                )}
+              </>
             )}
           </div>
         ))}
@@ -81,18 +182,28 @@ export const SurveyForm = ({ survey }: SurveyFormProps) => {
         <Label htmlFor="satisfaction">
           How satisfied are you with our product and services?
         </Label>
-        <Select name="satisfaction">
-          <SelectTrigger className="w-full rounded-md border-input bg-background flex justify-between items-center px-3 py-2 text-foreground shadow-sm">
-            <SelectValue placeholder="Select an option" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="very-dissatisfied">Very Dissatisfied</SelectItem>
-            <SelectItem value="dissatisfied">Dissatisfied</SelectItem>
-            <SelectItem value="neutral">Neutral</SelectItem>
-            <SelectItem value="satisfied">Satisfied</SelectItem>
-            <SelectItem value="very-satisfied">Very Satisfied</SelectItem>
-          </SelectContent>
-        </Select>
+        <Controller
+          name="satisfaction"
+          control={control}
+          rules={{ required: 'Satisfaction is required' }}
+          render={({ field }) => (
+            <Select onValueChange={field.onChange} value={field.value}>
+              <SelectTrigger className="w-full rounded-md border-input bg-background flex justify-between items-center px-3 py-2 text-foreground shadow-sm">
+                <SelectValue placeholder="Select an option" />
+              </SelectTrigger>
+              <SelectContent>
+                {Object.keys(SATISFACTION).map((key) => (
+                  <SelectItem key={key} value={key}>
+                    {LABEL_MAP[key as satisfaction]}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          )}
+        />
+        {errors.satisfaction && (
+          <p className="error_message">{errors?.satisfaction?.message?.toString()}</p>
+        )}
       </div>
       <div className="space-y-2">
         <Label htmlFor="feedback">
@@ -100,11 +211,12 @@ export const SurveyForm = ({ survey }: SurveyFormProps) => {
         </Label>
         <Textarea
           id="feedback"
-          name="feedback"
           rows={4}
           placeholder="Enter your feedback"
+          {...register('feedback')}
         />
       </div>
+      <Button type="submit">Submit</Button>
     </form>
   );
 };
---
+++ config/index.ts
@@ -0,0 +1,7 @@
+export const SATISFACTION = {
+  VERY_SATISFIED: 'Very Satisfied',
+  SATISFIED: 'Satisfied',
+  NEUTRAL: 'Neutral',
+  DISSATISFIED: 'Dissatisfied',
+  VERY_DISSATISFIED: 'Very Dissatisfied',
+} as const;

03519be891c789a1eb6b0a1d7b5eb837cd4f29e0
orzarhi <or.zarhi11@gmail.com>
2024-08-08 23:08:35 +0300

    chore: Update Navbar component and add Providers component
    You are updating the Navbar component to use a smaller logo size and adjust text color for better visual consistency. Additionally, you are implementing lazy loading for images in the component to improve page load performance.
    
    You are also adding a new "Providers" component that wraps other components to provide query client and theme capabilities.
    
    These changes focus on improving the UI consistency and performance of the application.

---
+++ components/providers.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactNode } from 'react';
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: ProvidersProps) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        enableSystem={true}
+        defaultTheme="system"
+      >
+        {children}
+      </NextThemesProvider>
+    </QueryClientProvider>
+  );
+};
